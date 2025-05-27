bd20b6c325695673ecbf72ea1b687c000b43404a
Mindcore M <tima.042007@mail.ru>
2025-05-28 03:04:19 +0600

    add router initialization for notes feature in __init__.py

---
+++ Timur_Medikhanov_Task_1/api/__init__.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from core.config import settings
+from note.views import router as router_note
+
+
+router = APIRouter(
+    prefix= settings.api.prefix
+)
+
+
+router.include_router(router_note,prefix=settings.api.note.prefix)
