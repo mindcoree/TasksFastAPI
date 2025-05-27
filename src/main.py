ffa5df051bf44d6e780fab66da8811f76fd17797
Mindcore M <tima.042007@mail.ru>
2025-05-27 23:40:14 +0600

    add lifespan context manager for FastAPI to manage database connections

--- src/main.py
+++ src/main.py
@@ -2,14 +2,31 @@ import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
+from contextlib import asynccontextmanager
+from core import db_helper
 
-app = FastAPI()
-app.include_router(api_router, prefix=settings.api.prefix)
+
+# Для корректного закрыть всех соединения с БД
+# Освободить, ресурсы (файлы, сокеты, кэши и т.д.)
+# Короче для безопасности
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup — если нужно, можно что-то подключить
+    yield
+    # shutdown
+    await db_helper.dispose()
+    print("dispose engine")
+
+
+main_app = FastAPI(
+    lifespan=lifespan,
+)
+main_app.include_router(api_router, prefix=settings.api.prefix)
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
