a138e88a60b73118a757a79ef292a6044793e7ef
Mindcore M <tima.042007@mail.ru>
2025-05-28 01:44:52 +0600

    add naming convention to Base metadata configuration

--- src/core/base.py
+++ src/core/base.py
@@ -1,6 +1,12 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
+from core.config import settings
 
 
 class Base(DeclarativeBase):
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convertion,
+    )
+
     id: Mapped[int] = mapped_column(primary_key=True)
