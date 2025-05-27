40b734d08a92eef497a7efcf70c0d2231ae96889
Mindcore M <tima.042007@mail.ru>
2025-05-27 21:27:42 +0600

    configure FastAPI app with API router and dynamic settings

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
 import uvicorn
-
+from fastapi import FastAPI
+from api import router as api_router
+from core.config import settings
 
 app = FastAPI()
+app.include_router(api_router, prefix=settings.api.prefix)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
