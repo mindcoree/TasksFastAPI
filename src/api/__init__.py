57acb81cec4073199fd34646db086c4963e884c4
Mindcore M <tima.042007@mail.ru>
2025-05-28 02:11:55 +0600

    add user router to main API router with prefix from settings

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
+from users.views import router as router_users
+from core.config import settings
 
 router = APIRouter()
+
+router.include_router(router_users,prefix=settings.api.users.prefix)
