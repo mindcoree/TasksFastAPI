a7ced0daab02226b6020ce05afba272427f168a5
Mindcore M <tima.042007@mail.ru>
2025-05-27 22:43:11 +0600

    add DatabaseHelper class for asynchronous database interactions

---
+++ src/core/db_helper.py
@@ -0,0 +1,50 @@
+from typing import AsyncGenerator
+from config import settings
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+)
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        max_overflow: int = 10,  # КОЛИЧЕСТВО СЕССИИ
+        pool_size: int = 5,  # количество соединений в пуле
+    ):
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            expire_on_commit=False,  # Данные сохраняются в памяти после commit
+        )
+
+    # Не обязательно делать. Закрывает соединения и удаляет пул.
+    # При работе с uvicorn это делается автоматически, но для graceful shutdown — полезно.
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
+)
