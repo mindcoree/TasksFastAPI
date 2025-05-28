8824dc2ed2c47fae9e8a685dc5d726edef1af11d
Mindcore M <tima.042007@mail.ru>
2025-05-28 16:06:00 +0600

    refactor API router initialization for note prefix

--- Timur_Medikhanov_Task_1/api/__init__.py
+++ Timur_Medikhanov_Task_1/api/__init__.py
@@ -3,9 +3,7 @@ from core.config import settings
 from note.views import router as router_note
 
 
-router = APIRouter(
-    prefix= settings.api.prefix
-)
+router = APIRouter(prefix=settings.api.note.prefix)
 
 
-router.include_router(router_note,prefix=settings.api.note.prefix)
+router.include_router(router_note, prefix=settings.api.note.prefix)
