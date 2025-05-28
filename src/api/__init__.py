6375e1dd8a9eb99bc1267a49f287f113766bf0a4
Mindcore M <tima.042007@mail.ru>
2025-05-28 18:14:36 +0600

    fix: update import paths for users views and core config

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from users.views import router as router_users
-from core.config import settings
+from src.api.users.views import router as router_users
+from src.core.config import settings
 
 router = APIRouter()
 
-router.include_router(router_users,prefix=settings.api.users.prefix)
+router.include_router(router_users, prefix=settings.api.users.prefix)
