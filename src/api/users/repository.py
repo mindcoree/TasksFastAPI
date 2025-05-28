9d3d00656f4d88f51ac501c23fd93cff81e8b684
Mindcore M <tima.042007@mail.ru>
2025-05-28 18:13:41 +0600

    feat: add UserRepository for user data access with async methods

---
+++ src/api/users/repository.py
@@ -0,0 +1,24 @@
+from fastapi import Depends, FastAPI
+from typing import Sequence
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from src.api.users.schemas import UserCraete
+
+from src.core.user import User
+
+
+class UserRepository:
+    def __init__(self, session):
+        self.session: AsyncSession = session
+
+    async def get_users(self) -> Sequence[User]:
+        stms = select(User)
+        result = await self.session.scalars(stms)
+        return result.all()
+
+    async def create_user(self, user_in: UserCraete) -> User:
+        create_user = User(**user_in.model_dump())
+        self.session.add(create_user)
+        await self.session.commit()
+        await self.session.refresh(create_user)
+        return create_user
