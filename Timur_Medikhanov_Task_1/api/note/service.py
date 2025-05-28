7b66df715ea2f61ac4f3aecae35f2d3a1d3e731d
Mindcore M <tima.042007@mail.ru>
2025-05-28 19:26:15 +0600

    feat: implement NotesService for managing notes and interacting with the repository

---
+++ Timur_Medikhanov_Task_1/api/note/service.py
@@ -0,0 +1,16 @@
+from Timur_Medikhanov_Task_1.api.note.repository import NotesRepository
+from typing import Sequence
+from Timur_Medikhanov_Task_1.api.note.schemas import NoteCreate, NoteOut
+
+
+class NotesService:
+    def __init__(self, repository: NotesRepository):
+        self.repo: NotesRepository = repository
+
+    async def get_list_notes(self) -> Sequence[NoteOut]:
+        notes = await self.repo.get_list_notes()
+        return notes
+
+    async def create_notes(self, note_in) -> NoteCreate:
+        create_note = await self.repo.create_notes(note_in)
+        return create_note
