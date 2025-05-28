f83890a7affac4a7225e352f123ef491a1fa0621
Mindcore M <tima.042007@mail.ru>
2025-05-28 19:24:39 +0600

    fix: update import paths for core config and note views

--- Timur_Medikhanov_Task_1/api/__init__.py
+++ Timur_Medikhanov_Task_1/api/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from core.config import settings
-from note.views import router as router_note
+from Timur_Medikhanov_Task_1.core.config import settings
+from Timur_Medikhanov_Task_1.api.note.views import router as router_note
 
 
-router = APIRouter(prefix=settings.api.note.prefix)
+router = APIRouter()
 
 
 router.include_router(router_note, prefix=settings.api.note.prefix)
