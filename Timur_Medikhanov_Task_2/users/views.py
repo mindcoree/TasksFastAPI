647c9c91b0130acafadbe409138919a032443c88
Mindcore M <tima.042007@mail.ru>
2025-05-31 19:54:42 +0600

    feat: update user login and registration endpoints to improve user handling

--- Timur_Medikhanov_Task_2/users/views.py
+++ Timur_Medikhanov_Task_2/users/views.py
@@ -3,15 +3,16 @@ from core.db_helper import SessionDep
 from users import repository as repo
 from .schemas import UserLogin, UserCreate, ResponseUser
 from .models import User
+from .dependencies import CheckUniqueUsers
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/login")
 async def user_login(session: SessionDep, user: UserLogin):
-    return await repo.get_users(session=session, get_user=user)
+    return await repo.sign_in(session=session, sign_in_user=user)
 
 
 @router.post("/register", response_model=ResponseUser)
-async def user_register(session: SessionDep, reg_user: UserCreate):
+async def user_register(session: SessionDep, reg_user: CheckUniqueUsers):
     return await repo.create_users(session=session, user_in=reg_user)
