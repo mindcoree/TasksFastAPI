83693febd28e07548cae8d374b15913f8f75ab9b
Mindcore M <tima.042007@mail.ru>
2025-05-31 22:59:59 +0600

    fix: correct UserCreate schema reference in create-user endpoint

--- src/api/users/views.py
+++ src/api/users/views.py
@@ -2,7 +2,7 @@ from typing import Sequence
 
 from fastapi import APIRouter, status
 from .dependencies import UserServiceDep
-from .schemas import UserRead, UserCraete
+from .schemas import UserRead, UserCreate
 
 
 router = APIRouter(tags=["users"])
@@ -15,7 +15,7 @@ async def get_users(service: UserServiceDep) -> Sequence[UserRead]:
 
 
 @router.post(
-    "/create-user", response_model=UserCraete, status_code=status.HTTP_201_CREATED
+    "/create-user", response_model=UserCreate, status_code=status.HTTP_201_CREATED
 )
-async def create_user(service: UserServiceDep, user_in: UserCraete):
+async def create_user(service: UserServiceDep, user_in: UserCreate):
     return await service.created_user(user_in=user_in)
