b3da52f28ca1893e268dc98cae8eebda541e7839
Mindcore M <tima.042007@mail.ru>
2025-05-31 22:57:49 +0600

    feat: add JWT encoding and decoding functions with key management

--- Timur_Medikhanov_Task_4/utils/auth.py
+++ Timur_Medikhanov_Task_4/utils/auth.py
@@ -1,4 +1,50 @@
 import bcrypt
+import jwt
+from datetime import datetime, timezone, timedelta
+from functools import lru_cache
+
+from sqlalchemy.sql.coercions import expect
+
+from core.config import settings
+
+
+@lru_cache(maxsize=1)
+def get_private_key() -> str:
+    return settings.auth.private_key.read_text()
+
+
+@lru_cache(maxsize=1)
+def get_public_key() -> str:
+    return settings.auth.publib_key.read_text()
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = get_private_key(),
+    algorithm: str = settings.auth.algorithm,
+    expire_minute: str = settings.auth.expire_min,
+):
+    now = datetime.now(timezone.utc)
+    expire = now + timedelta(minutes=expire_minute)
+
+    payload.update(
+        exp=int(expire.timestamp()),
+        iat=int(now.timestamp()),
+    )
+
+    return jwt.encode(
+        payload=payload,
+        key=private_key,
+        algorithm=algorithm,
+    )
+
+
+def decode_jwt(
+    token: str,
+    public_key: str = get_public_key(),
+    algorithm: str = settings.auth.algorithm,
+):
+    return jwt.decode(jwt=token, key=public_key, algorithms=[algorithm])
 
 
 def hash_password(password: str) -> str:
