0e0714592a27689420fbe00f360a6793d6eadc84
Mindcore M <tima.042007@mail.ru>
2025-05-31 20:31:56 +0600

    feat: add status code to user registration endpoint for clarity

--- Timur_Medikhanov_Task_2/users/views.py
+++ Timur_Medikhanov_Task_2/users/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from core.db_helper import SessionDep
 from users import repository as repo
 from .schemas import UserLogin, UserCreate, ResponseUser
@@ -13,6 +13,10 @@ async def user_login(session: SessionDep, user: UserLogin):
     return await repo.sign_in(session=session, sign_in_user=user)
 
 
-@router.post("/register", response_model=ResponseUser)
+@router.post(
+    "/register",
+    response_model=ResponseUser,
+    status_code=status.HTTP_201_CREATED,
+)
 async def user_register(session: SessionDep, reg_user: CheckUniqueUsers):
     return await repo.create_users(session=session, user_in=reg_user)
