34f61add65f63989e61693ac7a2ab7795357758c
Mindcore M <tima.042007@mail.ru>
2025-05-31 22:58:32 +0600

    feat: add user credential verification function for login

--- Timur_Medikhanov_Task_4/users/dependencies.py
+++ Timur_Medikhanov_Task_4/users/dependencies.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 from fastapi import Depends
-from users.schemas import UserCreate
+
+from users.schemas import UserCreate, UserLogin, ResponseUser
 from sqlalchemy import select, Result
+
+from utils import auth
 from .models import User
 from fastapi import HTTPException, status
 from core.db_helper import SessionDep
@@ -20,3 +23,23 @@ async def check_unique_user(session: SessionDep, check_user: UserCreate) -> User
 
 
 CheckUniqueUsers = Annotated[UserCreate, Depends(check_unique_user)]
+
+
+async def verify_credentials(session: SessionDep, sign_in_user: UserLogin) -> UserLogin:
+    users = select(User).where(User.username == sign_in_user.username)
+    result: Result = await session.execute(users)
+    user = result.scalars().first()
+
+    if not user or not auth.verify_password(
+        password=sign_in_user.password,
+        hashed_password=user.password,
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password",
+        )
+
+    return user
+
+
+VerificationUser = Annotated[ResponseUser, Depends(verify_credentials)]
