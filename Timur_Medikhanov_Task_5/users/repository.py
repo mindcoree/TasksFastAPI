9795afd5cc5a37a283da86c48c58f3acf68e3749
Mindcore M <tima.042007@mail.ru>
2025-06-01 02:56:10 +0600

    feat: add get_user func

--- Timur_Medikhanov_Task_5/users/repository.py
+++ Timur_Medikhanov_Task_5/users/repository.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy import select, Result
 from utils import auth
-from .schemas import ResponseUser, UserCreate, AccessToken
+from .schemas import ResponseUser, UserCreate, AccessToken, UserLogin
 from .models import User
 
 
@@ -15,8 +15,15 @@ async def create_users(session: AsyncSession, user_in: UserCreate) -> ResponseUs
 
 async def sign_in(user_login: ResponseUser) -> AccessToken:
     payload = {
-        "sub": user_login.id,
+        "sub": str(user_login.id),
         "username": user_login.username,
     }
     token = auth.encode_jwt(payload=payload)
     return AccessToken(token=token, token_type="Cookie")
+
+
+async def get_user(session: AsyncSession, user: UserLogin) -> User:
+    users = select(User).where(User.username == user.username)
+    result: Result = await session.execute(users)
+    user = result.scalars().first()
+    return user
