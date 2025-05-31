b5caeef14185f0d080b07fb88af03ebecd1ce1d8
Mindcore M <tima.042007@mail.ru>
2025-06-01 02:56:55 +0600

    feat: add endpoint protected func info_me

--- Timur_Medikhanov_Task_5/users/views.py
+++ Timur_Medikhanov_Task_5/users/views.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, status, Response
 from core.db_helper import SessionDep
 from users import repository as repo
 from .schemas import ResponseUser, AccessToken
-from .dependencies import VerificationUser
-from .dependencies import CheckUniqueUsers
+from .dependencies import VerificationUser, CheckUniqueUsers, CurrentUser
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -11,6 +11,16 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/login", response_model=AccessToken)
 async def login(credentials: VerificationUser, response: Response):
     access_token = await repo.sign_in(user_login=credentials)
+
+    response.set_cookie(
+        key="access_token",
+        value=access_token.token,
+        samesite="lax",
+        secure=False,
+        httponly=True,
+        max_age=30 * 60,
+    )
+
     return access_token
 
 
@@ -21,3 +31,9 @@ async def login(credentials: VerificationUser, response: Response):
 )
 async def user_register(session: SessionDep, reg_user: CheckUniqueUsers):
     return await repo.create_users(session=session, user_in=reg_user)
+
+
+@router.get("/me", response_model=ResponseUser)
+async def info_user(session: SessionDep, current_user: CurrentUser):
+    user = await repo.get_user(session=session, user=current_user)
+    return ResponseUser(id=user.id, username=user.username)
