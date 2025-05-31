cdf0471265bd5d7cde871de0748691382b3b3a6d
Mindcore M <tima.042007@mail.ru>
2025-05-31 19:54:55 +0600

    feat: refactor user creation and sign-in logic with password hashing

--- Timur_Medikhanov_Task_2/users/repository.py
+++ Timur_Medikhanov_Task_2/users/repository.py
@@ -1,29 +1,41 @@
+from sqlalchemy import select, Result
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from utils import auth
 from .schemas import UserLogin, ResponseUser, UserCreate
-from .dependencies import CheckUniqueUsers
 from .models import User
-from sqlalchemy import select, Result
 
 
-async def create_users(
-    session: AsyncSession, user_in: CheckUniqueUsers
-) -> ResponseUser:
-    new_user = User(**user_in.model_dump())
+async def create_users(session: AsyncSession, user_in: UserCreate) -> ResponseUser:
+    hash_password = auth.hash_password(user_in.password)
+    new_user = User(username=user_in.username, password=hash_password)
     session.add(new_user)
     await session.commit()
     return ResponseUser(id=new_user.id, username=new_user.username)
 
 
-async def get_users(session: AsyncSession, get_user: UserLogin) -> dict:
-    users = select(User).where(get_user.password == User.password)
+async def sign_in(session: AsyncSession, sign_in_user: UserLogin) -> dict:
+    users = select(User).where(User.username == sign_in_user.username)
     result: Result = await session.execute(users)
-    check_auth = result.scalars().first()
-    if not check_auth:
+    user = result.scalars().first()
+
+    if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="The user is unauthorized",
         )
+
+    verify_password = auth.verif_password(
+        password=sign_in_user.password,
+        hashed_password=user.password,
+    )
+
+    if not verify_password:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not the right password",
+        )
+
     return {
         "complete": True,
         "message": "The user is authorized",
