832f5a12c7c183b99c99e9e4f708b0d025cd4cb4
Mindcore M <tima.042007@mail.ru>
2025-06-02 02:46:47 +0600

    feat: enhance JWT creation with flexible expiration handling and new token functions

--- Timur_Medikhanov_Task_5/utils/auth.py
+++ Timur_Medikhanov_Task_5/utils/auth.py
@@ -3,6 +3,8 @@ import jwt
 from datetime import datetime, timezone, timedelta
 from functools import lru_cache
 from core.config import settings
+from type.jwt import TOKEN_TYPE_FIELD, REFRESH_TOKEN_TYPE, ACCESS_TOKEN_TYPE
+from users.schemas import ResponseUser
 
 
 @lru_cache(maxsize=1)
@@ -19,10 +21,15 @@ def encode_jwt(
     payload: dict,
     private_key: str = get_private_key(),
     algorithm: str = settings.auth.algorithm,
-    expire_minute: int = settings.auth.expire_min,
+    expire_minutes: int = settings.auth.access_token_expire_min,
+    expire_timedelta: timedelta | None = None,
 ):
     now = datetime.now(timezone.utc)
-    expire = now + timedelta(minutes=expire_minute)
+
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
 
     payload.update(
         exp=int(expire.timestamp()),
@@ -55,4 +62,42 @@ def verify_password(password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         password=password.encode(),
         hashed_password=hashed_password.encode(),
+    )
+
+
+def create_jwt(
+    token_data: dict,
+    token_type: str,
+    expire_minutes: int = settings.auth.access_token_expire_min,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    payload = {TOKEN_TYPE_FIELD: token_type}
+    payload.update(token_data)
+    return encode_jwt(
+        payload=payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+    )
+
+
+def create_access_token(user_info: ResponseUser) -> str:
+    payload = {
+        "sub": str(user_info.id),
+        "username": user_info.username,
+    }
+    return create_jwt(
+        token_type=ACCESS_TOKEN_TYPE,
+        token_data=payload,
+        expire_minutes=settings.auth.access_token_expire_min,
+    )
+
+
+def create_refresh_token(user_info: ResponseUser) -> str:
+    payload = {
+        "sub": str(user_info.id),
+    }
+    return create_jwt(
+        token_type=REFRESH_TOKEN_TYPE,
+        token_data=payload,
+        expire_timedelta=timedelta(days=settings.auth.refresh_token_expire_days),
     )
