702ef60767dae444c7242b071fede1bcc732901c
Mindcore M <tima.042007@mail.ru>
2025-06-03 02:48:42 +0600

    feat: update login and user info endpoints to support async methods and enhanced token handling

--- Timur_Medikhanov_Task_5/users/views.py
+++ Timur_Medikhanov_Task_5/users/views.py
@@ -1,38 +1,49 @@
-from fastapi import APIRouter, Response, status
-from .schemas import TokenInfo, ResponseUser
+from fastapi import (
+    APIRouter,
+    Response,
+    status,
+    Request,
+    HTTPException,
+)
 from .dependencies import (
     UserAuthServiceDep,
     VerificationUser,
     CheckUniqueUsers,
     CurrentUser,
+    CurrentAdmin,
 )
+from core.config import settings
+from utils import auth
+from .schemas import TokenInfo, UserSchemas, UserPayload
 
 router = APIRouter()
 
 
 @router.post("/login", response_model=TokenInfo)
-def login(
+async def login(
     credentials: VerificationUser,
-    response: Response,
     user_auth_service: UserAuthServiceDep,
+    response: Response,
 ) -> TokenInfo:
-    access_token = user_auth_service.sign_in(user_login=credentials)
-
-    response.set_cookie(
+    user_login = await user_auth_service.sign_in(user_login=credentials)
+    await auth.set_token_cookie(
+        response=response,
         key="access_token",
-        value=access_token.token,
-        samesite="lax",
-        secure=False,
-        httponly=True,
-        max_age=30 * 60,
+        value=user_login.access,
+        max_age=settings.auth.access_token_expire_min * 60,
     )
-
-    return access_token
+    await auth.set_token_cookie(
+        response=response,
+        key="refresh_token",
+        value=user_login.refresh,
+        max_age=settings.auth.refresh_token_expire_days * 24 * 60 * 60,
+    )
+    return user_login
 
 
 @router.post(
     "/register",
-    response_model=ResponseUser,
+    response_model=UserSchemas,
     status_code=status.HTTP_201_CREATED,
 )
 async def user_register(
@@ -42,6 +53,11 @@ async def user_register(
     return await user_auth_service.create_user(user_in=reg_user)
 
 
-@router.get("/me", response_model=ResponseUser)
+@router.get("/me", response_model=UserPayload)
 async def info_user(current_user: CurrentUser):
-    return current_user
+    return UserPayload(**current_user)
+
+
+@router.get("/me/admin", response_model=UserPayload)
+async def info_user(current_admin: CurrentAdmin):
+    return UserPayload(**current_admin)
