701ffb548fdef1025669cc91f07dc9ea761c4720
Mindcore M <tima.042007@mail.ru>
2025-06-03 02:48:25 +0600

    feat: enhance user authentication flow with async methods and payload handling

--- Timur_Medikhanov_Task_5/users/dependencies.py
+++ Timur_Medikhanov_Task_5/users/dependencies.py
@@ -1,12 +1,13 @@
 from typing import Annotated
-from fastapi import Depends, Form, Request
-from .schemas import UserCreate, UserLogin, ResponseUser
+from fastapi import Depends, Form, Request, Response, HTTPException, status
+from .schemas import UserCreate, UserLogin, UserSchemas, UserPayload
 from .services import UserAuthService
 from .repository import UserRepository
 from core.db_helper import SessionDep
+from utils import auth
 
 
-def get_user_auth_service(session: SessionDep) -> UserAuthService:
+async def get_user_auth_service(session: SessionDep) -> UserAuthService:
     repository = UserRepository(session=session)
     return UserAuthService(repository=repository)
 
@@ -27,18 +28,59 @@ CheckUniqueUsers = Annotated[UserCreate, Depends(check_unique_user)]
 async def verify_credentials(
     sign_in_user: Annotated[UserLogin, Form()],
     user_auth_service: UserAuthServiceDep,
-) -> ResponseUser:
+) -> UserSchemas:
     return await user_auth_service.verify_credentials(sign_in_user=sign_in_user)
 
 
-VerificationUser = Annotated[ResponseUser, Depends(verify_credentials)]
+VerificationUser = Annotated[UserSchemas, Depends(verify_credentials)]
 
 
-def get_current_user(
+async def access_token_payload(
     request: Request,
+    response: Response,
     user_auth_service: UserAuthServiceDep,
-) -> ResponseUser:
-    return user_auth_service.get_current_user(request=request)
+) -> dict:
+    payload = getattr(request.state, "user_payload", None)
+    if payload:
+        return payload
+
+    refresh_token = request.cookies.get("refresh_token")
+    if not refresh_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh-token is not provided",
+        )
+
+    user = await user_auth_service.refresh_access_token_and_get_user(
+        refresh_token=refresh_token, response=response
+    )
+    payload = auth.create_payload(payload_user=user)
+    return payload
 
 
-CurrentUser = Annotated[ResponseUser, Depends(get_current_user)]
+AccessTokenPyload = Annotated[dict, Depends(access_token_payload)]
+
+
+async def get_current_user(payload: AccessTokenPyload) -> dict:
+    role = payload.get("role")
+    if role != "user":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="FORBIDDEN User"
+        )
+    return payload
+
+
+CurrentUser = Annotated[UserPayload, Depends(get_current_user)]
+# CurrentUser = Annotated[dict, Depends(get_current_user)]
+
+
+async def get_current_admin(payload: AccessTokenPyload) -> dict:
+    role = payload.get("role")
+    if role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="FORBIDDEN admin "
+        )
+    return payload
+
+
+CurrentAdmin = Annotated[UserPayload, Depends(get_current_admin)]
