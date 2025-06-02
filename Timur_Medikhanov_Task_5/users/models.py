3a6e83ad832662d9a0030d6d4228dc629735c8e2
Mindcore M <tima.042007@mail.ru>
2025-06-03 02:49:03 +0600

    feat: add Role enum and role field to User model with default value

--- Timur_Medikhanov_Task_5/users/models.py
+++ Timur_Medikhanov_Task_5/users/models.py
@@ -1,5 +1,14 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import expression
+
 from core.base import Base
+from enum import Enum
+
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    guest = "guest"
 
 
 class User(Base):
@@ -8,3 +17,6 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
+    role: Mapped[Role] = mapped_column(
+        server_default=expression.literal("guest"), default=Role.guest
+    )
