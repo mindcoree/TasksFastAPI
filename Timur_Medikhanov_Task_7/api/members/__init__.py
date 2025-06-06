d25e1ffc34974acc4475650b9b7e0f9d161ae450
Mindcore M <tima.042007@mail.ru>
2025-06-06 20:11:27 +0600

    feat: include admin router in API for enhanced member management

--- Timur_Medikhanov_Task_7/api/members/__init__.py
+++ Timur_Medikhanov_Task_7/api/members/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from core.config import settings
 from .users.views import router as users_router
+from .admins.views import router as admins_router
 
 router = APIRouter(prefix=settings.api.members.prefix)
 
 
 router.include_router(users_router)
+router.include_router(admins_router)
