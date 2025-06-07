ed906baa98432b25767e9bdedd7953c38c926ad8
Mindcore M <tima.042007@mail.ru>
2025-06-07 22:54:10 +0600

    feat: add OrderStatus and PaymentStatus enums for order and payment management

--- Timur_Medikhanov_Task_7/api/common/enums.py
+++ Timur_Medikhanov_Task_7/api/common/enums.py
@@ -4,3 +4,17 @@ from enum import Enum
 class Role(str, Enum):
     ADMIN = "admin"
     USER = "user"
+
+
+class OrderStatus(str, Enum):
+    PENDING = "pending"  # заказа создан, но не обработан
+    PROCESSING = "processing"  # в обработке
+    SHIPPED = "shipped"  # Отправлен
+    DELIVERED = "delivered"  # Доставлен
+    CANCELLED = "cancelled"  # Отменен
+
+
+class PaymentStatus(str, Enum):
+    PENDING = "pending"
+    PAID = "paid"
+    FAILED = "failed"
