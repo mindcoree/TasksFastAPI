0b8c20097823be22d9f0527053f07263a316ac53
Mindcore M <tima.042007@mail.ru>
2025-06-07 22:51:35 +0600

    feat: add orders router and import necessary models for SQLAlchemy initialization

--- Timur_Medikhanov_Task_7/api/__init__.py
+++ Timur_Medikhanov_Task_7/api/__init__.py
@@ -2,9 +2,18 @@ from fastapi import APIRouter
 from core.config import settings
 from .members import router as members_router
 from .products.views import router as products_router
+from .orders.views import router as orders_router
+
+# Импорт моделей для инициализации SQLAlchemy-схем
+from api.members.models import Member
+from api.orders.models import Order
+from api.products.models import Product
+from api.common.order_product_association import OrderProductAssociation
+
 
 router = APIRouter(prefix=settings.api.prefix)
 
 
 router.include_router(members_router)
 router.include_router(products_router)
+router.include_router(orders_router)
