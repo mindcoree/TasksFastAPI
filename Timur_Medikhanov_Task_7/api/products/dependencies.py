0720b56485bbc1bf6a7f1df8b168cac260754b66
Mindcore M <tima.042007@mail.ru>
2025-06-07 22:57:19 +0600

    feat: add dependency injection for ProductService and implement get_product_or_404 function

--- Timur_Medikhanov_Task_7/api/products/dependencies.py
+++ Timur_Medikhanov_Task_7/api/products/dependencies.py
@@ -1,5 +1,6 @@
 from core.db_helper import SessionDep
 from api.common.dependencies import get_service
+from .schemas import ProductOut
 from .services import ProductService
 from .repository import ProductRepository
 from typing import Annotated
@@ -15,3 +16,13 @@ async def get_product_service(session: SessionDep) -> ProductService:
 
 
 ProductServiceDep = Annotated[ProductService, Depends(get_product_service)]
+
+
+async def get_product_or_404(
+    product_id: int,
+    service: ProductServiceDep,
+) -> ProductOut:
+    return await service.ensure_instance_exists_by_id(id_instance=product_id)
+
+
+ExistingProduct = Annotated[ProductOut, Depends(get_product_or_404)]
