d8a83e7fbfb8669ef3012fcb273a5e4753d75560
Mindcore M <tima.042007@mail.ru>
2025-06-07 22:54:34 +0600

    feat: refactor product endpoints to use ExistingProduct dependency for improved clarity

--- Timur_Medikhanov_Task_7/api/products/views.py
+++ Timur_Medikhanov_Task_7/api/products/views.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status
 from core.config import settings
-from .dependencies import ProductServiceDep
+from .dependencies import ProductServiceDep, ExistingProduct
 from .schemas import ProductOut, ProductIn
 from type.annotated import form_model
 from api.members.admins.dependencies import AdminRestricted
@@ -11,11 +11,10 @@ router = APIRouter(prefix=settings.api.products.prefix, tags=["products REST"])
 
 @router.get("/product/{product_id}", response_model=ProductOut)
 async def get_product(
-    product_id: int,
-    service: ProductServiceDep,
     restricted: AdminRestricted,
+    product_entity: ExistingProduct,
 ) -> ProductOut:
-    return await service.get_product_by_id(product_id)
+    return product_entity
 
 
 @router.post(
@@ -24,9 +23,9 @@ async def get_product(
     status_code=status.HTTP_201_CREATED,
 )
 async def create_product(
+    restricted: AdminRestricted,
     product: form_model(ProductIn),
     service: ProductServiceDep,
-    restricted: AdminRestricted,
 ) -> ProductOut:
 
     return await service.create_product(product_in=product)
@@ -37,21 +36,21 @@ async def create_product(
     response_model=ProductOut,
 )
 async def update_product(
-    product_id: int,
+    product_entity: ExistingProduct,
+    restricted: AdminRestricted,
     product: form_model(ProductIn),
     service: ProductServiceDep,
-    restricted: AdminRestricted,
 ) -> ProductOut:
-    return await service.update_product_by_id(product_id, product_in=product)
+    return await service.update_product_by_id(product_entity.id, product_in=product)
 
 
 @router.delete(
     "/delete/{product_id}",
 )
 async def delete_product(
-    service: ProductServiceDep,
-    product_id: str,
+    product_entity: ExistingProduct,
     restricted: AdminRestricted,
+    service: ProductServiceDep,
 ) -> dict:
-    await service.delete_product_by_id(delete_id=product_id)
+    await service.delete_product_by_id(delete_id=product_entity.id)
     return {"successfully": True}
