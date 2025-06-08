189a936cc36ac830365b09972f58e51e098e3fdb
Mindcore M <tima.042007@mail.ru>
2025-06-09 01:28:02 +0600

    feat: add MemberRelationMix for handling member relationships in SQLAlchemy models

--- Timur_Medikhanov_Task_7/api/common/mixins.py
+++ Timur_Medikhanov_Task_7/api/common/mixins.py
@@ -1,5 +1,8 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, declared_attr, relationship
+
 from api.common.enums import Role
 from type.annotated import created_at, updated_at
 
@@ -17,3 +20,28 @@ class TimestampMix:
 
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
+
+
+if TYPE_CHECKING:
+    from api.members.models import Member
+
+
+class MemberRelationMix:
+    __member_id_unique__: bool = False  # для связей o2o
+    __member_nullable__: bool = False
+    __member_back_populates: str | None = None
+
+    @declared_attr
+    def member_id(cls) -> Mapped[int]:
+        return mapped_column(
+            ForeignKey(
+                "members.id",
+                ondelete="CASCADE",
+            ),
+            nullable=cls.__member_nullable__,
+            unique=cls.__member_id_unique__,
+        )
+
+    @declared_attr
+    def member(cls) -> Mapped["Member"]:
+        return relationship("Member", back_populates=cls.__member_back_populates)
