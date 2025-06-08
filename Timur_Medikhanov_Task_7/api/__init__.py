b25eda7389ca63c3db7979aee494c435301e3bcc
Mindcore M <tima.042007@mail.ru>
2025-06-09 01:30:05 +0600

    feat: reorganize imports in __init__.py for improved structure

--- Timur_Medikhanov_Task_7/api/__init__.py
+++ Timur_Medikhanov_Task_7/api/__init__.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
+
 from core.config import settings
-from .members import router as members_router
-from .products.views import router as products_router
-from .orders.views import router as orders_router
 
 # Импорт моделей для инициализации SQLAlchemy-схем
+from api.common.order_product_association import OrderProductAssociation
 from api.members.models import Member
 from api.orders.models import Order
 from api.products.models import Product
-from api.common.order_product_association import OrderProductAssociation
+from api.bank.models import BankAccount
 
+from .members import router as members_router
+from .orders.views import router as orders_router
+from .products.views import router as products_router
 
 router = APIRouter(prefix=settings.api.prefix)
 
--- Timur_Medikhanov_Task_7/api/members/__init__.py
+++ Timur_Medikhanov_Task_7/api/members/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
+
 from core.config import settings
-from .users.views import router as users_router
 from .admins.views import router as admins_router
+from .users.views import router as users_router
 
 router = APIRouter(prefix=settings.api.members.prefix)
 
