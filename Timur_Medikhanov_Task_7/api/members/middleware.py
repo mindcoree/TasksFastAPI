d7bbc9d63cd98ce4ef191f42d6168f79ebb3f096
Mindcore M <tima.042007@mail.ru>
2025-06-09 01:28:19 +0600

    feat: update JWT middleware to use member_payload instead of user_payload

--- Timur_Medikhanov_Task_7/api/members/middleware.py
+++ Timur_Medikhanov_Task_7/api/members/middleware.py
@@ -4,27 +4,28 @@ from fastapi import Request, HTTPException, status, Response
 from jwt.exceptions import InvalidTokenError
 
 from main import main_app
+from type.jwt import ACCESS_TOKEN_TYPE
 from utils import auth
 from .validations import validations_token_type
-from type.jwt import ACCESS_TOKEN_TYPE
 
 
 @main_app.middleware("http")
 async def verify_jwt_middleware(
-    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+    request: Request,
+    call_next: Callable[[Request], Awaitable[Response]],
 ):
     public_paths = {"/login", "/register", "/refresh", "/docs", "/openapi.json"}
     if request.url.path in public_paths:
         return await call_next(request)
 
     token = request.cookies.get("access_token")
-    request.state.user_payload = None
+    request.state.member_payload = None
 
     if token:
         try:
             payload = await auth.decode_jwt(token=token)
             await validations_token_type(ACCESS_TOKEN_TYPE, payload)
-            request.state.user_payload = payload
+            request.state.member_payload = payload
         except InvalidTokenError:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
